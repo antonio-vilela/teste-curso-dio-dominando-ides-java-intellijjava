c652fb072623b7b79e71f0f5116dc926fb9c503b
Perkles <kykovilela@gamil.com>
2021-12-21 09:34:05 -0300

    first commit

---
+++ src/br/com/dia/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dia;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+      /*int a = 5;
+        int b = 3;
+        System.out.print("Hello World!" + (a+b));*/
+    }
+}
+
+
+
